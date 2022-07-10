083598e4b9256c95cf0093a7a3a2fdac1b518235
ali terohid <a.terohid@alis-MacBook-Pro.local>
2022-07-10 15:05:03 +0430

    complete support

--- src/Components/Support/Support.js
+++ src/Components/Support/Support.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState } from 'react';
 
 //styles
 import styles from "./Support.module.scss"
@@ -9,9 +9,25 @@ import RecordProblem_RaiseQuestion from './RecordProblem-RaiseQuestion/RecordPro
 import ContactUs from "./ContactUs/ContactUs";
 
 const Support = () => {
+
+    const [ component , setComponennt ] = useState("ContactUs")
+
+    const clickHandler = ( name ) => {
+        setComponennt( name )
+    }
+
     return (
-        <div className={ styles.support } >
-            <ContactUs />
+        <div style={{ background: "#fff" }} >
+            <div className={ styles.support } >
+                <div className={ styles.header }>
+                    <h3 className={ component == "ContactUs" ? styles.title : null } onClick={ () => clickHandler( "ContactUs" ) } >تماس با ما</h3>
+                    <h3 className={ component == "CreditReturnRequest" ? styles.title : null } onClick={ () => clickHandler( "CreditReturnRequest" ) } >درخواست بازگشت اعتبار</h3>
+                    <h3 className={ component == "RecordProblem_RaiseQuestion" ? styles.title : null } onClick={ () => clickHandler( "RecordProblem_RaiseQuestion" ) } >ثبت مشکل یا طرح پرسش</h3>
+                </div>
+                { component == "ContactUs" ? <ContactUs className={ styles.component } /> : null }
+                { component == "CreditReturnRequest" ? <CreditReturnRequest className={ styles.component } /> : null }
+                { component == "RecordProblem_RaiseQuestion" ? <RecordProblem_RaiseQuestion className={ styles.component } /> : null }
+            </div>
         </div>
     );
 };
